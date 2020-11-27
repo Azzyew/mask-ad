4329fec7ed09c358fe55c466b7293c353cd60dd9
azzyew <laisadnevescosta@gmail.com>
2020-11-27 14:44:39 -0300

    fixing grammatical errors

--- src/content/como.jsx
+++ src/content/como.jsx
@@ -1,3 +1,3 @@
-const como = "É recomendado o uso de máscaras que possuem duas camadas de tecido, essa proteção acontece porque a máscara cria uma barreira física contra o coronavírus, impedindo a proliferação e ajudando a reduzir o número de pessoas infectadas. As recomendações de como utilizar a máscara são:";
+const como = "É recomendado o uso de máscaras que possuem duas camadas de tecido. A proteção pela máscara acontece porque ela cria uma barreira física contra o coronavírus, impedindo a proliferação e ajudando a reduzir o número de pessoas infectadas. As recomendações de como utilizar a máscara são:";
 
 export default como;
